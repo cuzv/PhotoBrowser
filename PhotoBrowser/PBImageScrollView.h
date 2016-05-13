ce001c94406ac38759297f0d77350fc9df3e0d8f
Shaw <cuzval@gmail.com>
2016-05-13 19:59:07 +0800

    prepare fix swipe bug

---
+++ PhotoBrowser/PBImageScrollView.h
@@ -0,0 +1,15 @@
+//
+//  PBImageScrollView.h
+//  PhotoBrowser
+//
+//  Created by Moch Xiao on 5/12/16.
+//  Copyright © 2016 Moch Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBImageScrollView : UIScrollView <UIScrollViewDelegate>
+@property (nonatomic, strong) UIImageView *imageView;
+
+- (void)setImage:(UIImage *)image;
+@end
